f6cd818fcbc19c5c46f87c0f1bab80ef18702504
Fidelia <fidelia_okandze@hotmail.co.uk>
2021-09-08 23:17:22 +0100

    new component dictionary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import logo from "./logo.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <a href="#" className="btn btn primary shadow">
-          hello
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <a href="#" className="btn btn primary shadow">
+            hello
+          </a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Fidelia Okandze</footer>
+      </div>
     </div>
   );
 }
